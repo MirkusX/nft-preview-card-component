62d59c55972f47c93756c2b8b6c68be62117583e
MirkusX <marcusk.kodehode@gmail.com>
2023-03-13 13:53:41 +0100

    html structure

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,8 +1,24 @@
+import equilibrium from "../Images/equilibrium.jpg";
+
 export const Card = () => {
   return (
     <section>
       <div>
-        <h1>Card</h1>
+        <div>
+          <img src={equilibrium} />
+        </div>
+        <h1>Equilibrium #3429</h1>
+        <p>
+          Our Equilibrium collection promotes bankruptcy and environmental
+          disaster
+        </p>
+        <div>
+          <p>0.041 ETH</p>
+          <p>3 days left</p>
+        </div>
+        <div>
+          <p>Creation of Jules Wyvern</p>
+        </div>
       </div>
     </section>
   );
